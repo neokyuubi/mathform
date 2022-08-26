95be0195778f3e1c30f5a718a57f52b18cb14968
Neokyuubi <neokyuubi@gmail.com>
2022-08-26 15:40:45 +0000

    Math Validators

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MathValidators } from '../math-validators';
 
 @Component({
   selector: 'app-equation',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquationComponent implements OnInit {
 
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber),
+    b: new FormControl(this.randomNumber),
+    answer: new FormControl("")
+  },
+  [MathValidators.addition('answer','a','b')]);
+
   constructor() { }
 
-  ngOnInit(): void {
+  get a():number
+  {
+    return this.mathForm.value.a;
+  }
+
+  get b():number
+  {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit(): void 
+  {
+    this.mathForm.statusChanges.subscribe((value)=>
+    {
+      if(value === 'INVALID')
+      {
+        return;
+      }
+
+      this.mathForm.controls["a"].setValue(this.randomNumber);
+      this.mathForm.controls["b"].setValue(this.randomNumber);
+      this.mathForm.controls["answer"].setValue('');
+
+      this.mathForm.setValue(
+      {
+        a: this.randomNumber,
+        b:this.randomNumber,
+        answer: '',
+      }
+      );
+    })
+  }
+
+  get randomNumber()
+  {
+    return Math.floor(Math.random() * 10);
   }
 
 }
